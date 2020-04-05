c816beca7bf0fed5276b017099ea6ce0f867115a
MKS1030 <manish10mks@gmail.com>
2020-04-05 19:54:38 +0530

    basic intial ..

---
+++ JS/Basic/array_sort.js
@@ -0,0 +1,5 @@
+let arr = [1,43,24,32,5,2]
+
+let result  = arr.sort( (a,b) => a - b);
+
+console.log(result);
---
+++ JS/Basic/array_sum.js
@@ -0,0 +1,5 @@
+let arr = [2,4,8,2,1];
+
+let result  =  arr.reduce( (sum,current) => sum + current);
+
+console.log(result);
