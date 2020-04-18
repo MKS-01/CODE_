016ca8b4d2dcde814631add15b99628237a3f654
MKS-01 <manish10mks@gmail.com>
2020-04-18 14:15:41 +0530

    cheat sheet

--- JS/Basic/array.js
+++ JS/Basic/array.js
@@ -1,7 +1,35 @@
+//variable
+let abc = 1;
+var xyz = "an";
+
+//constant
+const qwe = "any";
+
+//comments: support single line as well
+/*multiline
+ comments
+ */
+
+/*The "use strict" directive switches the engine to the “modern” mode,
+ changing the behavior of some built-in features.
+ supports by all the  modern browser.
+
+ */
+
+//type conversion
+let num = 12;
+console.log("Number  to String:", String(num)); //num to string
+console.log("Number to String", num.toString(), typeof num.toString());
+
+let str = "125";
+console.log("String to Number:", Number(str)); //str to num
+
+console.log("Boolean conversion:", Boolean(str));
+console.log("Boolean conversion:", Boolean(""));
+
 //declaration
 // let arr = new Array();
 // let arr = [];
-
 let arr = [1, 6, 2, 8, 10, 2];
 
 console.log(arr);
